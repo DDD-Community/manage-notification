d6e4b4e2b68555994ddcbb7bd61da5f00cef0bb9
integerJI <jjs9536@gmail.com>
2024-11-30 15:17:11 +0900

    feat: email send add email tamplate

--- emailsend/views.py
+++ emailsend/views.py
@@ -6,6 +6,7 @@ from notification.serializers import NotificationSerializer
 from project.utils import custom_response
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
+from emailsend.models import EmailTemplate
 
 class SendEmailView(APIView):
     
@@ -13,26 +14,33 @@ class SendEmailView(APIView):
         operation_description="이메일 발송 API",
         operation_summary="이메일 발송 API",
         responses={
-            200: NotificationSerializer(many=True),  # 성공 시 발송된 Notification 객체 리스트 반환
-            400: "Bad Request"  # 실패 시 응답 코드
+            200: NotificationSerializer(many=True),
+            400: "Bad Request"
         }
     )
     def post(self, request):
-        # email_sent가 0인 Notification 객체들 조회
         notifications = Notification.objects.filter(email_sent=0)
 
-        # 이메일 발송
+        email_template = EmailTemplate.objects.order_by('-created_at').first()
+        if not email_template:
+            return custom_response(
+                data={"error": "사용할 수 있는 이메일 템플릿이 없습니다."},
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
+
         for notification in notifications:
+            subject = email_template.title
+            html_message = email_template.body.replace("{name}", notification.name)  # body에서 {name} 변환
+
             send_mail(
-                subject="Notification",
-                message=f"안녕하세요, {notification.name}님. 여기 당신의 알림이 있습니다.",
+                subject=subject,
+                message="HTML 지원이 필요합니다. HTML 템플릿을 확인하세요.",
                 from_email=settings.DEFAULT_FROM_EMAIL,
                 recipient_list=[notification.email],
+                html_message=html_message
             )
-            # email_sent 필드를 1로 업데이트
             notification.email_sent = 1
             notification.save()
 
-        # 발송된 객체들을 시리얼라이즈하여 반환
         serializer = NotificationSerializer(notifications, many=True)
         return custom_response(data=serializer.data, status_code=status.HTTP_200_OK)
